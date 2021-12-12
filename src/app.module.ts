141c2e0faa24fca16455c2750b179de7d8250f13
Altuğ Buğra Çelenk <altugbugracelenk@Altugs-MBP.home>
2021-12-12 15:04:01 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,44 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { MailController } from './app.controller';
+import { MailService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          port: config.get('MAIL_PORT'),
+          secure: false,
+          auth: {
+            user: config.get('MAIL_USER'),
+            pass: config.get('MAIL_PASSWORD'),
+          },
+          tls: {
+            rejectUnauthorized: false,
+            minVersion: 'TLSv1',
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${config.get('MAIL_FROM')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [MailController],
+  providers: [MailService],
+})
+export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { MailParamsDto } from './types/mail.params.dto';
+import { Subject, Template } from './types/mail.enum';
+import { returnContext } from './helpers/return-context';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendMail(data: MailParamsDto) {
+    try {
+      const context = returnContext(data);
+      return await this.mailerService.sendMail({
+        to: data.user.email,
+        subject: Subject[data.mailType],
+        template: Template[data.mailType],
+        context,
+      });
+    } catch (error: any) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
